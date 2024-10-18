b8d3b0b69f1244ffb941d2dab6293ac61aeb6a45
Zhenya Holub <zhenya.holub@rdigital.by>
2024-10-18 15:30:52 +0300

    feat: create validateUser function and User type

--- src/methods/createUser.ts
+++ src/methods/createUser.ts
@@ -1,12 +1,15 @@
 import { IncomingMessage, ServerResponse } from 'http';
 
 import { parseBody } from '../utils/helpers.js';
+import { validateUser } from '../utils/validate.js';
+import { User } from '../types/user.js';
 
 export const createUser = async (
     req: IncomingMessage,
     res: ServerResponse<IncomingMessage> & { req: IncomingMessage }
 ) => {
-    const body = await parseBody(req);
+    const body = (await parseBody(req)) as User;
+    const isValidUser = validateUser(body);
 
     res.end();
 };
--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -1,1 +1,14 @@
-export const validateUser = () => {};
+import { User } from '../types/user.js';
+
+export const validateUser = (fields: User) => {
+    const rules = {
+        username: (data: unknown) => typeof data === 'string' && data.trim(),
+        age: (data: unknown) => data && typeof data === 'number',
+        hobbies: (data: unknown) =>
+            Array.isArray(data) &&
+            data.every((item) => typeof item === 'string')
+    };
+    return Object.entries(fields).every(([key, value]) =>
+        rules[key as keyof typeof rules](value)
+    );
+};
